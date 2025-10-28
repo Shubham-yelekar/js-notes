29c35a20d36be26e2c86b6693e71a7e7bba90d15
Shubham Yelekar <70461738+Shubham-yelekar@users.noreply.github.com>
2025-10-28 12:12:40 +0530

    day -2

---
+++ 01-Fundamentals/Day-2/script.js
@@ -0,0 +1,22 @@
+let studentName = "Shubham";
+let age = 8;
+let isStudent = true;
+let lang = "Javscript";
+
+const Data = [
+  {
+    studentName: "Shubham",
+    age: 28,
+    isStudent: true,
+    lang: "Javscript",
+  },
+  {
+    studentName: "Archana",
+    age: 25,
+    isStudent: true,
+    lang: "Python",
+  },
+];
+
+Data[0].age = 24;
+console.log(Data);
